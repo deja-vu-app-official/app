733f90f2b8859f401e28a1bc246d3164fd2fba17
Alexnoff <121128262+Deja-vu-app@users.noreply.github.com>
2022-12-20 22:09:47 +0100

    Adds Secured API calls
    Implemented code to make a simple API call to our strapi.
    
    Call has to be made with the API TOKEN or it will deny it

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, FlatList} from 'react-native';
+import {API_TOKEN} from '@env'
+ 
+export default class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource:[]
+     };
+   }
+ 
+  componentDidMount(){
+    fetch("https://deja-vu-api.herokuapp.com/api/places",{
+      method: 'get',
+      headers: new Headers({
+        'Authorization': `Bearer ${API_TOKEN}`, 
+      }),  
+    })
+    .then(response => response.json())
+    .then((responseJson)=> {
+      this.setState({
+       dataSource: responseJson.data
+      })
+    })
+    .catch(error=>console.log(error)) //to catch the errors if any
+    }
+ 
+    render(){
+     return(
+      <View style={{padding:10}}>
+      <FlatList
+      padding ={30}
+         data={this.state.dataSource}
+         renderItem={({item}) => 
+         <View style={{height: 50}}>
+          <Text style={{height: 50}}>{item.attributes.city}, {item.attributes.address}</Text>
+          <View style={{height: 1,backgroundColor:'gray'}}></View>
+         </View>
+        }
+       />
+      
+     </View>
+     )}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
